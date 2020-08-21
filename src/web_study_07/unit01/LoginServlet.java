a54cbc135c283e0a6d735d79c03e7b256e6155b2
teacher <net94@naver.com>
2020-08-21 14:33:19 +0900

    el을 이용한 로그인 폼/servlet 적용

---
+++ src/web_study_07/unit01/LoginServlet.java
@@ -0,0 +1,31 @@
+package web_study_07.unit01;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    process(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    String id = request.getParameter("id");
+	    String pwd = request.getParameter("pwd");
+	    
+	    request.setAttribute("id", id);
+	    request.setAttribute("pwd", pwd);
+	    
+	    request.getRequestDispatcher("04_testLogin_servlet.jsp").forward(request, response);
+    }
+}
